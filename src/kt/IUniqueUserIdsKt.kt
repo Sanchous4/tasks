6ecdd6801f69b43bc2f8cc01f8e735b00b3e61fa
Sani <pilyugaa4@gmail.com>
2026-01-02 16:18:27 +0300

    MyHashMap.java

--- src/kt/IUniqueUserIdsKt.kt
+++
@@ -1,8 +0,0 @@
-package kt
-
-interface IUniqueUserIdsKt {
-    fun count(userIds: IntArray): Int
-    fun uniqueIds(userIds: IntArray): IntArray
-    fun uniqueSortedIds(userIds: IntArray): IntArray
-    fun uniqueIdsWithPreservedOrder(userIds: IntArray): IntArray
-}
